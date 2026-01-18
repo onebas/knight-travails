b59dd9cdd312387cc02893a0d43e20df1e8ce8e8
one <216416666+onebas@users.noreply.github.com>
2026-01-18 16:55:48 +0530

    Create basic BFS graph traversal for finding paths

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+import { findNeighbors } from "./Neighbors.js";
+
+function shortestPath(a, b){
+    let queue = [];
+    let paths = new Map();
+    let visited = new Set();
+
+    if(a.join() == b.join()) return a;
+
+    queue.push(a);
+    visited.add(a);
+
+    while(queue.length!=0){
+        let vertex = queue.shift();
+
+        if(vertex.join() == b.join()){
+            break;
+            // functionality for creating path to be added in a different module
+        }
+
+        let neighbors = findNeighbors(vertex);
+        for(let neighbor of neighbors){
+            if(!(visited.has(neighbor))){
+                visited.add(neighbor);
+                queue.push(neighbor);
+                paths.set(neighbor, vertex);
+            }
+        }
+    }
+}   
 
 
-
+shortestPath([3, 3], [4, 3]);
